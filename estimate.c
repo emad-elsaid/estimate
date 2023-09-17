5a83d2ade49eb400724117a7e8e6e4ffdf3247e7
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2023-09-18 00:19:13 +0200

    some setup changes

--- estimate.c
+++ estimate.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-#define PAGE                                                                   \
-  "<html><head><title>libmicrohttpd demo</title></head><body>libmicrohttpd "   \
-  "demo</body></html>"
+#define PAGE "Hello world"
 
 typedef enum HTTP_METHOD {
   HTTP_METHOD_INVALID,
@@ -13,7 +12,7 @@ typedef enum HTTP_METHOD {
   HTTP_METHOD_POST
 } Method;
 
-typedef int HTTP_Status;
+typedef int Status;
 
 typedef struct Request {
   Method method;
@@ -21,11 +20,11 @@ typedef struct Request {
 } Request;
 
 typedef struct Response {
-  HTTP_Status status;
+  Status status;
   void *body;
 } Response;
 
-void HelloWorld(Response *w, const Request *r) {
+void root(Response *w, const Request *r) {
   w->body = PAGE;
   w->status = MHD_HTTP_OK;
 }
@@ -40,20 +39,30 @@ Method StringToHTTPMethod(const char *method) {
   return HTTP_METHOD_INVALID;
 }
 
-static enum MHD_Result Router(void *cls, struct MHD_Connection *connection,
+bool PathIs(const Request *r, const char *path) {
+  return strcmp(r->path, path) == 0;
+}
+
+void Router(Response *w, const Request *r) {
+  bool is_GET = r->method == HTTP_METHOD_GET;
+  bool is_POST = r->method == HTTP_METHOD_POST;
+
+  if (is_GET && PathIs(r, "/")) return root(w, r);
+}
+
+static enum MHD_Result AccessCallback(void *cls, struct MHD_Connection *connection,
                                 const char *url, const char *method_str,
                                 const char *version, const char *upload_data,
                                 size_t *upload_data_size, void **ptr) {
 
-  Request *r = (Request*) calloc(1, sizeof(Request));
-  r->method = StringToHTTPMethod(method_str);
+  Method method = StringToHTTPMethod(method_str);
 
   // unexpected method
-  if (r->method == HTTP_METHOD_INVALID)
+  if (method == HTTP_METHOD_INVALID)
     return MHD_NO;
 
   // upload data in a GET!?
-  if (r->method == HTTP_METHOD_GET && *upload_data_size != 0)
+  if (method == HTTP_METHOD_GET && *upload_data_size != 0)
     return MHD_NO;
 
   static int dummy;
@@ -64,18 +73,30 @@ static enum MHD_Result Router(void *cls, struct MHD_Connection *connection,
     return MHD_YES;
   }
 
-  r->path = url;
-
   // clear context pointer
   *ptr = NULL;
-  Response *w = (Response*) calloc(1, sizeof(Response));
 
-  HelloWorld(w, r);
+  Request *r = (Request *)calloc(1, sizeof(Request));
+  r->method = method;
+  r->path = url;
 
-  struct MHD_Response *response = MHD_create_response_from_buffer(strlen(w->body), w->body, MHD_RESPMEM_PERSISTENT);
+  Response *w = (Response *)calloc(1, sizeof(Response));
+
+  Router(w, r);
+  if( w->status == 0 ) {
+    free(r);
+    free(w);
+    return MHD_NO;
+  }
+
+  struct MHD_Response *response = MHD_create_response_from_buffer(
+      strlen(w->body), w->body, MHD_RESPMEM_PERSISTENT);
   enum MHD_Result ret = MHD_queue_response(connection, w->status, response);
 
   MHD_destroy_response(response);
+
+  free(r);
+  free(w);
   return ret;
 }
 
@@ -87,7 +108,7 @@ int main(int argc, char **argv) {
 
   int port = atoi(argv[1]);
 
-  struct MHD_Daemon *d = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION, port, NULL, NULL, &Router, NULL, MHD_OPTION_END);
+  struct MHD_Daemon *d = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION, port, NULL, NULL, &AccessCallback, NULL, MHD_OPTION_END);
   if (d == NULL) return 1;
 
   printf("Server started on port %d", port);
